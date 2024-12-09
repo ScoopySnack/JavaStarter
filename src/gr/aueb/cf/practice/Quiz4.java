7f897c0c5d974afa3e51197d1e4a6c172a717200
ScoopySnack <atzy.niko@gmail.com>
2024-12-09 17:31:06 +0200

    new staff

---
+++ src/gr/aueb/cf/practice/Quiz4.java
@@ -0,0 +1,24 @@
+package gr.aueb.cf.practice;
+
+import java.util.Scanner;
+
+public class Quiz4 {
+
+    public static void main(String[] args) {
+        char ch = '!';
+        switch (ch) {
+            case 'H':
+            case '*':
+            case '!':
+            case '&':
+                System.out.println("Hello");
+                break;
+            default:
+                System.out.println("Adios");
+                break;
+        }
+
+
+
+    }
+}
