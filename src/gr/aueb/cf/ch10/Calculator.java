a271e34f3a4ba0b6664e8b778cbf51f1efe3ea91
Angeliki Nikolaou <atzy.niko@gmail.com>
2024-12-10 20:01:31 +0200

    update

---
+++ src/gr/aueb/cf/ch10/Calculator.java
@@ -0,0 +1,18 @@
+package gr.aueb.cf.ch10;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+
+    
+}
---
+++ src/gr/aueb/cf/ch5/challenges/MaxNumbers.java
@@ -0,0 +1,4 @@
+package gr.aueb.cf.ch5.challenges;
+
+public class MaxNumbers {
+}
