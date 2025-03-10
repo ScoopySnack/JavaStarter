d680857d141dd916c734f778a5e82932d26758a5
Angeliki Nikolaou <atzy.niko@gmail.com>
2025-03-10 18:37:18 +0200

    feat: add Point, Point3D, ImmutablePoint, and Point3DImmutable classes for 3D point representation

---
+++ src/gr/aueb/cf/ch14/immutable/Point3DImmutable.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.ch14.immutable;
+
+public class Point3DImmutable extends ImmutablePoint{
+    private final ImmutablePoint immutablePoint;
+    private final int z;
+
+    public Point3DImmutable() {
+        immutablePoint = new ImmutablePoint();
+        z = 0;
+    }
+
+    public Point3DImmutable(ImmutablePoint immutablePoint, int z) {
+        this.immutablePoint = immutablePoint;
+        this.z = z;
+    }
+
+    public ImmutablePoint getImmutablePoint() {
+        return immutablePoint;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    //We don't need setters because the class is immutable
+}
