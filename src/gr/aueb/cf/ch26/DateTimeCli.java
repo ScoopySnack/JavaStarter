9d79b395715aa5db84cd816e9c405cc61e1a639b
Angeliki Nikolaou <atzy.niko@gmail.com>
2025-03-19 18:00:00 +0200

    feat: implement concurrent server for handling multiple client connections

---
+++ src/gr/aueb/cf/ch26/DateTimeCli.java
@@ -0,0 +1,47 @@
+package gr.aueb.cf.ch26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class DateTimeCli {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        Socket socket = null;
+        BufferedReader bf = null;
+
+        try {
+            InetAddress servAddress = InetAddress.getByName("time.nist.gov");
+            //InetAddress is a class that represents an IP address
+            int servPort = 13;
+
+            socket = new Socket(servAddress, servPort);
+            bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //We use a BufferedReader to read the response from the server.
+
+            String line = "";
+
+            //We need a loop to read the response line by line
+            while ((line = bf.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+
+            System.out.println("Daytime server response: " + sb.toString());
+
+        }catch (IOException e) {
+//            System.out.println("Cannot connect to the server");
+//            System.exit(1);
+            e.printStackTrace();
+        }finally {
+            try {
+                if (socket != null) socket.close();
+                if (bf != null) bf.close();
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/gr/aueb/cf/ch26/challenges/EchoServerApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.cf.ch26.challenges;
+
+import gr.aueb.cf.ch26.EchoServ;
+
+public class EchoServerApp {
+
+    public static void main(String[] args) {
+        EchoServ es = new EchoServ();
+        es.start();
+    }
+}
