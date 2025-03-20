f9591888480daec07f5277d9e059377a2d03302f
Angeliki Nikolaou <atzy.niko@gmail.com>
2025-03-20 22:09:50 +0200

    feat: add City and Trainee classes with cloning capabilities

--- src/gr/aueb/cf/ch17/knight/KnightMain.java
+++ src/gr/aueb/cf/ch17/knight/KnightMain.java
@@ -1,4 +1,16 @@
 package gr.aueb.cf.ch17.knight;
 
 public class KnightMain {
+
+    private static final Runnable joa = new Knight("Joa");
+    private static final Runnable whiteKnight = new Knight("White-Knight");
+    private static final Runnable saintGeorge = new Knight("Saint George");
+
+    public static void main(String[] args) {
+        (new Thread(joa)).start();
+        (new Thread(whiteKnight)).start();
+        (new Thread(saintGeorge)).start();
+
+
+    }
 }
--- src/gr/aueb/cf/ch17/knight/MissionStatus.java
+++ src/gr/aueb/cf/ch17/knight/MissionStatus.java
@@ -1,5 +1,8 @@
 package gr.aueb.cf.ch17.knight;
 
+/**
+ * Enums are used to represent a fixed set of constants.
+ */
 public enum MissionStatus {
     STARTED,
     NOT_STARTED
