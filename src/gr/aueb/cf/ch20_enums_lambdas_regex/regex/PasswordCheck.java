71eaa1a7e57de631c8abbaeb12e58012775d7209
Angeliki Nikolaou <atzy.niko@gmail.com>
2025-05-02 13:57:11 +0300

    feat: implement interactive password validation with user input

--- src/gr/aueb/cf/ch20_enums_lambdas_regex/regex/PasswordCheck.java
+++ src/gr/aueb/cf/ch20_enums_lambdas_regex/regex/PasswordCheck.java
@@ -1,21 +1,40 @@
 package gr.aueb.cf.ch20_enums_lambdas_regex.regex;
 
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * User gives a password and the program checks if it is valid.
+ * A valid password must contain at least:
+ * - 8 characters
+ * - 1 lowercase letter
+ * - 1 uppercase letter
+ * - 1 digit
+ * - 1 special character (#?!@$%^&*-)
+ */
 public class PasswordCheck {
 
     public static void main(String[] args) {
-        String password = "Abc1234!@";
-        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).{8,}$");
+        Scanner sc = new Scanner(System.in);
+        Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#?!@$%^&*-]).{8,}$");
         // ^ - start of string, (?=.*[a-z]) - at least one lowercase letter, (?=.*[A-Z]) - at least one uppercase letter,
         // (?=.*\\d) - at least one digit, (?=.*[@#$%^&+=!]) - at least one special character, .{8,} - at least 8 characters
-        Matcher matcher = pattern.matcher(password);
 
-        while (matcher.find()){
-            System.out.println(matcher.group());
-            System.out.println("Password is valid"); //
+        while (true) {
+            System.out.print("Enter password: ");
+            String password = sc.nextLine();
+            Matcher matcher = pattern.matcher(password);
+
+            if (matcher.find()) {
+                System.out.println("Password is valid");
+                break; // exit loop when password is valid
+            } else {
+                System.out.println("Password is invalid. Try again.");
+            }
+
         }
 
+        sc.close();
     }
 }
